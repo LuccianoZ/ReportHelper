f6f8bccc24427139bae5f503f1f3ed88688044b9
LuccianoZ <labziminsky@gmail.com>
2026-05-24 11:10:11 -0400

    Add project files.

---
+++ ReportHelper.Tests/Unit/SmokeTest.cs
@@ -0,0 +1,10 @@
+namespace ReportHelper.Tests.Unit;
+
+public class SmokeTest 
+{
+    [Fact]
+    public void XUnit_IsWired_AndTestRunnerSeesThisTest() 
+    {
+        Assert.True(true);
+    }
+}
